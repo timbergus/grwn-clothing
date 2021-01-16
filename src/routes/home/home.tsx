62b4e3575a994c17ea6ecbf7a7ad17783bf7998d
Gustavo Muñoz <timbergus@gmail.com>
2021-01-17 00:41:36 +0100

    Add first route and Sass.

---
+++ src/routes/home/home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1>HATS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
