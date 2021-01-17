5b0f7027c7906a266f310031f7cc0c864279ad4e
Gustavo Muñoz <timbergus@gmail.com>
2021-01-17 18:49:56 +0100

    Add header and app logo.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,9 +5,11 @@ import { Route } from 'react-router-dom';
 import Home from '../../pages/home/home';
 import Shop from '../../pages/shop/shop';
 import Contact from '../../pages/contact/contact';
+import Header from '../header/header';
 
 export const App = () => (
   <>
+    <Header />
     <Route exact path="/" component={Home} />
     <Route path="/shop" component={Shop} />
     <Route path="/contact" component={Contact} />
